f912560214659c6d1cd7ab2848154f61ac0cd4a9
KevinmUwU <148278136+KevinmUwU@users.noreply.github.com>
2023-12-05 11:04:20 -0600

    Add files via upload

---
+++ boceto.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>INSA &reg; </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/insa.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fuentes de google-->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  <!-- style for php boceto -->
+ <style>
+        #container1 {
+      display: flex;
+      flex: 1;
+    }
+
+
+    #contenedorIzquierdo {
+        
+  width: 1000px;
+
+    }
+
+.infoizq {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.texto-clickeable {
+    text-decoration: none; /* Elimina el subrayado del enlace */
+    cursor: pointer; /* Cambia el cursor al pasar sobre el enlace */
+    margin: 5px; /* Ajusta el margen entre los textos */
+
+    font-size: 1.5rem !important;
+    color: #000000 !important;
+}
+.texto-clickeable:hover {
+    font-weight: bold; /* Peso de la fuente en negrita al pasar el mouse */
+    background-color: rgba(169, 169, 169, 0.5); /* Color gris opaco al pasar el mouse */
+    border-radius: 5px; /* Bordes redondeados para suavizar la apariencia */
+}
+
+
+    #contenedoresDerechos {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  
+}
+
+.contenedorDerecho {
+  flex: 0 0 calc(33.33% - 10px); /* 33.33% para que haya 3 en una fila, y -10px para el margen */
+  border: 2px solid blue;
+  margin: 5px;
+  text-align: center;
+    height: 400px;
+ box-shadow: 0 0 20px #5E5E5E;
+}
+.enlace-imagen {
+    text-decoration: none; /* Elimina el subrayado del enlace */
+    cursor: pointer; /* Cambia el cursor al pasar sobre el enlace */
+    width: 30%;
+    height: 40%;
+}
+
+#contentb {
+    background-color: #F1ECEC; 
+   border-bottom: 2px solid black;
+}
+
+
+    </style>
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        
+      </div>
+      <div class="social-links d-none d-md-block">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      
+    <a href="index.html" class="logo"><img src="assets/img/insa.png" alt="" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
+          <li><a class="nav-link scrollto" href="#about">conversaciones</a></li>
+          <li><a class="nav-link scrollto" href="#services">marketing</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">asistencia</a></li>
+          <li><a class="nav-link scrollto" href="#team">informes</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contáctanos</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+
+
+<br>
+<div id="contentb">
+<a class="navbar-brand" href="#" style="margin-left: 55px; font-size: 1.5rem; color: #00000; font-weight: bold;">Biblioteca de paneles</a>
+</div>
+<br><br><div id="container1">
+    <div id="contenedorIzquierdo">
+<div class="infoizq">
+        <a href="#" class="texto-clickeable"> Todos los paneles </a>
+        <a href="#" class="texto-clickeable"> CMS </a>
+        <a href="#" class="texto-clickeable"> Marketing </a>
+        <a href="#" class="texto-clickeable"> Ventas </a>
+        <a href="#" class="texto-clickeable"> Asistencia </a>
+
+    </div>
+</div>
+    <div id="contenedoresDerechos">
+      <div class="contenedorDerecho">
+
+ <center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/mas.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Panel en blanco</a>
+<br>
+Comienza con un panel y agrega los informes que necesites.</div>
+
+
+      <div class="contenedorDerecho"><center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/grafica.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Analiticas</a>
+<br>
+Comienza con un panel de analiticas.</div>
+
+
+      <div class="contenedorDerecho"><center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/ingreso.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Ingresos</a>
+<br>
+Comienza con un panel de ingresos.</div>
+
+
+      <div class="contenedorDerecho"><center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/noti.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Notificaciones</a>
+<br>
+Notifica o checa tus notificaciones .</div>
+
+
+      <div class="contenedorDerecho"><center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/casa.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Venta de inmobilaria</a>
+<br>
+Comienza a generar carga de venta de casas.</div>
+
+
+      <div class="contenedorDerecho"><center>   <a href="h.html" class="enlace-imagen">
+        <img class="enlace-imagen" src="img/marketing.png"  alt="Descripción de la imagen">
+    </a><center><br>
+<a class="navbar-brand" href="#" style="margin-left: 30px; font-size: 1.5rem; color: #00000; font-weight: bold;">Mariketing</a>
+<br>
+Comienza con un panel de marketing.</div>
+    </div>
+  </div>
+
+<br><br>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Acerca de</h3>
+              <p>
+                 <br>
+                 C.P. 76904. El Pueblito, Corregidora, Qro.<br><br>
+                <strong>Telefono:</strong> 442 826 9910<br>
+                <strong>Correo:</strong> contacto@consorcioinsa.com.mx<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Mas Info</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+              
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Servicios</h4>
+            <ul>
+              
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              
+            </ul>
+          </div>
+
+         
+          
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Day</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by Denedig</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader">
+    <img src="assets/img/pinsa.gif" alt="Cargando...">
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
